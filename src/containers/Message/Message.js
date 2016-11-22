bf25051171b9056f4d94e1c1ec69629e2a293ad3
zmeecer <zmeecer@gmail.com>
2016-11-22 23:30:19 +0300

    added react-router added containers (routes) for chat and message added yarn.lock file

---
+++ src/containers/Message/Message.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Message extends React.Component {
+  render() {
+    return (
+      <h2>Message</h2>
+    )
+  }
+}
+
+export default Message;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Router, Route, browserHistory } from 'react-router';
+import App from './containers/App/App';
+import Chat from './containers/Chat';
+import Message from './containers/Message';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="chat" component={Chat} />
+      <Route path="message" component={Message} />
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
